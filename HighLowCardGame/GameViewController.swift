8488a83b0f3a0f14dac6b9f3535d155f49fec2a3
Tobias Schaarschmidt <dev@tosc.dev>
2020-08-31 20:27:12 +0200

    Add LICENSE

--- HighLowCardGame/GameViewController.swift
+++ HighLowCardGame/GameViewController.swift
@@ -127,19 +127,6 @@ class GameViewController: UIViewController {
         
         self.present(gameOverAlert, animated: true)
     }
-    
-    
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
 }
 
 extension GameViewController {
